bc976085996f84b3f7fcc9d41c9e51c6ea425b87
Shem Jaoko <jaokoshem@gmail.com>
2022-05-16 11:19:02 +0300

    Update: update arrow dropdown in navbar
    Update arrow transition effect in navbar and add function to close all other dropdowns if one is open

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("dropdown").classList.toggle("show");
-}
-  
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-arrow')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+const links = document.querySelectorAll(".navbar-flex>li")
+links.forEach((el, i) => {
+  el.isOpen = false;
+  el.onclick = e => {
+    const dropdown = el.querySelector(".dropdown")
+    const arrow = el.querySelector(".dropdown-arrow")
+    el.isOpen ? close(dropdown,
+      arrow) : open(dropdown,
+      arrow)
+    el.isOpen = !el.isOpen
+    //Close other open dropdowns
+    links.forEach((el, j) => {
+      const dropdown = el.querySelector(".dropdown")
+      const arrow = el.querySelector(".dropdown-arrow")
+      if (i !== j) {
+        if (el.isOpen) {
+          close(dropdown, arrow)
+          el.isOpen = false
+        }
       }
-    }
+    })
   }
-}
+
+})
